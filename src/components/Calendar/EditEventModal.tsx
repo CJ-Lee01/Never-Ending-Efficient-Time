61459a2253e72fb6cf95224c1ccf98e6a440559d
richiehx <hsiehrichie246@gmail.com>
2023-05-31 13:43:42 +0800

    Created the UI for the calendar page.

---
+++ src/components/Calendar/EditEventModal.tsx
@@ -0,0 +1,91 @@
+import {
+  useDisclosure,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  Textarea,
+  VStack,
+} from "@chakra-ui/react";
+import { FC } from "react";
+import { FiEdit } from "react-icons/fi";
+
+const EditEventModal: FC = ({}) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <FiEdit
+        cursor="pointer"
+        className="text-blue-500"
+        size={20}
+        onClick={onOpen}
+      />
+
+      <Modal
+        closeOnOverlayClick={false}
+        isOpen={isOpen}
+        onClose={onClose}
+        isCentered
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Edit Event</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <Box m={4}>
+              <VStack spacing={5}>
+                <FormControl>
+                  <FormLabel>Event Title</FormLabel>
+                  <Input
+                    type="text"
+                    size="md"
+                    placeholder="Type Here"
+                    borderColor="#E0E1E7"
+                  />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Deadline</FormLabel>
+                  <InputGroup borderColor="#E0E1E7">
+                    <Input type="date" size="md" />
+                  </InputGroup>
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Description</FormLabel>
+                  <Textarea
+                    borderColor="gray.300"
+                    placeholder="Write your task description here"
+                  />
+                </FormControl>
+              </VStack>
+            </Box>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+              variant="solid"
+              bg="#0D74FF"
+              color="white"
+              _hover={{ bg: "blue.600" }}
+              mr={3}
+            >
+              Save
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default EditEventModal;
