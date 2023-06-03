810524de615085206b0c387066ec2d3eefc7c587
CJ-Lee01 <chunjie01@live.com>
2023-06-03 15:18:39 +0800

    Added list view functionality for events.

--- src/app/(User supposed to see if logged in)/calendar/page.tsx
+++ src/app/(User supposed to see if logged in)/calendar/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import CalendarGrid from "@/components/Calendar/CalendarGrid";
 import EventsList from "@/components/Calendar/EventsList";
+import { defaultEvent, getEvent } from "@/lib/CRUD_Calendar";
+import { eventInformation } from "@/lib/types";
 import {
   Grid,
   GridItem,
@@ -12,44 +14,71 @@ import {
   useColorModeValue,
   Textarea,
 } from "@chakra-ui/react";
+import { PostgrestError } from "@supabase/supabase-js";
+import { createContext, useEffect, useState } from "react";
+
+export const EventInfoContext = createContext<
+  {
+    events: eventInformation[],
+    pageUpdater: () => void,
+  }
+>({
+  events: [],
+  pageUpdater: () => { },
+});
 
 export default function CalendarPage() {
+  const [eventList, setEventList] = useState<{
+    data: eventInformation[] | null,
+    error: PostgrestError | null,
+  }>({
+    data: null,
+    error: null,
+  })
+
+  const [dummy, updateEvent] = useState<boolean>(false); //just a gimmick to ensure that the page is rerendered.
+  const pageUpdater = () => updateEvent(prevBool => !prevBool);
+
+  useEffect(() => {
+    getEvent(setEventList);
+  }, [dummy])
+
   return (
-    <Grid
-      gridTemplateRows={"1fr"}
-      gridTemplateColumns={{ base: "1fr", md: "1fr", xl: "6fr 2fr" }}
-      gap={6}
-      p={16}
-    >
-      <GridItem>
-        <CalendarGrid />
-      </GridItem>
-
-      <GridItem>
-        <Stack align={"center"}>
-          <Stack
-            borderRadius={15}
-            borderColor={useColorModeValue("blackAlpha.300", "whiteAlpha.500")}
-            borderWidth={1}
-            p={3}
-            mb={4}
-          >
-            <Stack px={{ base: 28, xl: 12 }} pb={10}>
-              <Image src="/images/NUSmods.png" alt="" />
-              <FormControl justifyContent="center">
-                <FormLabel>Enter your NUSmods timetable link:</FormLabel>
-                <Textarea size="lg" resize="none"></Textarea>
-              </FormControl>
+    <EventInfoContext.Provider value={{ events: eventList.data ?? [], pageUpdater: pageUpdater }}>
+      <Grid
+        gridTemplateRows={"1fr"}
+        gridTemplateColumns={{ base: "1fr", md: "1fr", xl: "6fr 2fr" }}
+        gap={6}
+        p={16}
+      >
+        <GridItem>
+          <CalendarGrid />
+        </GridItem>
+        <GridItem>
+          <Stack align={"center"}>
+            <Stack
+              borderRadius={15}
+              borderColor={useColorModeValue("blackAlpha.300", "whiteAlpha.500")}
+              borderWidth={1}
+              p={3}
+              mb={4}
+            >
+              <Stack px={{ base: 28, xl: 12 }} pb={10}>
+                <Image src="/images/NUSmods.png" alt="" />
+                <FormControl justifyContent="center">
+                  <FormLabel>Enter your NUSmods timetable link:</FormLabel>
+                  <Textarea size="lg" resize="none"></Textarea>
+                </FormControl>
+              </Stack>
             </Stack>
+            <Divider
+              borderColor={useColorModeValue("orange.300", "green.300")}
+              borderWidth={1}
+            />
+            <EventsList />
           </Stack>
-          <Divider
-            borderColor={useColorModeValue("orange.300", "green.300")}
-            borderWidth={1}
-          />
-
-          <EventsList />
-        </Stack>
-      </GridItem>
-    </Grid>
+        </GridItem>
+      </Grid>
+    </EventInfoContext.Provider>
   );
 }
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,26 +2,35 @@ import { Session, User } from "@supabase/supabase-js";
 import { ChangeEventHandler, ReactElement } from "react";
 
 export interface InputFormFieldProps {
-    type?: string,
-    changeHandler: ChangeEventHandler<HTMLInputElement>,
-    children?: React.ReactNode,
-    isRequired?: boolean,
+  type?: string,
+  changeHandler: ChangeEventHandler<HTMLInputElement>,
+  children?: React.ReactNode,
+  isRequired?: boolean,
 }
 
 export interface UserData {
-    user: User | null,
-    session: Session | null
+  user: User | null,
+  session: Session | null
 }
 
 export interface CardProps {
-    heading: string;
-    description: string;
-    icon: ReactElement;
-    href: string;
+  heading: string;
+  description: string;
+  icon: ReactElement;
+  href: string;
 }
 
 export interface featureDescription {
-    heading: string,
-    icon: string,
-    description: string
+  heading: string,
+  icon: string,
+  description: string
+}
+
+export interface eventInformation {
+  id?: string,
+  user_id?: string,
+  event_name: string,
+  event_description: string,
+  start_time: string,
+  end_time: string,
 }
