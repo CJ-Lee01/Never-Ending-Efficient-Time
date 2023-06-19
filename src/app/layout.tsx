bd41ff5b0e90d552e67ecd846d27c98f42b56859
Lee Chun Jie <109913135+CJ-Lee01@users.noreply.github.com>
2023-06-19 18:43:10 +0800

    Update layout.tsx
    Fixed code for deployment on Vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,10 +9,13 @@ import { theme } from "@/chakra/theme";
 
 const inter = Inter({ subsets: ["latin"] });
 
+/*
+This code is causing issues with deploying on vercel.
 export const metadata = {
   title: "NEET",
   description: "Manage your Time well",
 };
+*/
 
 export default function RootLayout({
   children,
