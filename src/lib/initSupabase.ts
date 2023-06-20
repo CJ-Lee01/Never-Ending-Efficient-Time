d011f23c1165777e269f8e19183148b8a3e73724
Lee Chun Jie <109913135+CJ-Lee01@users.noreply.github.com>
2023-06-20 22:28:00 +0800

    Update root URL of initSupabase.ts
    expanded root URL to allow other root URLs

--- src/lib/initSupabase.ts
+++ src/lib/initSupabase.ts
@@ -5,7 +5,7 @@ export const supabaseUser = () => createClient(
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ0aWxkb3Z4ZW5qeXp0Z3pmdmxhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ2NjgzODYsImV4cCI6MjAwMDI0NDM4Nn0.4EjdmCUIUYoXLJ5NHqqTove7fxAjGw1wdA0bb0MuV8c"
 );
 
-export const ROOT_URL = "http://localhost:3000";
+export const ROOT_URL = window.location.hostname;
 
 export function getAnyFromServer(table: string) {
   return async () => {
