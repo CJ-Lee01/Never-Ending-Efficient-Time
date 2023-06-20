0fac25efe02c6417360b5f76ddccadee6d081a0f
Lee Chun Jie <109913135+CJ-Lee01@users.noreply.github.com>
2023-06-20 22:36:42 +0800

    fixed root URL in initSupabase.ts
    reflected deployment URL.

--- src/lib/initSupabase.ts
+++ src/lib/initSupabase.ts
@@ -5,7 +5,7 @@ export const supabaseUser = () => createClient(
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ0aWxkb3Z4ZW5qeXp0Z3pmdmxhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ2NjgzODYsImV4cCI6MjAwMDI0NDM4Nn0.4EjdmCUIUYoXLJ5NHqqTove7fxAjGw1wdA0bb0MuV8c"
 );
 
-export const ROOT_URL = window.location.hostname;
+export const ROOT_URL = "https://never-ending-efficient-time.vercel.app/";
 
 export function getAnyFromServer(table: string) {
   return async () => {
