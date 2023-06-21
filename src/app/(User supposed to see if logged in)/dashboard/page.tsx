138a4714a3fc54c46c5febf670e85d421ba65bab
richiehx <hsiehrichie246@gmail.com>
2023-06-21 21:56:50 +0800

    Added the CircularProgress for the Dashboard Tasks Summary.

--- src/app/(User supposed to see if logged in)/dashboard/page.tsx
+++ src/app/(User supposed to see if logged in)/dashboard/page.tsx
@@ -2,20 +2,18 @@
 
 import ToDoSummary from "@/components/Dashboard/ToDoSummary";
 import Annoucements from "@/components/Dashboard/Annoucements";
-import { Flex, Stack, Heading } from "@chakra-ui/react";
+import { Stack, Heading } from "@chakra-ui/react";
 
 export default function dashboard() {
   return (
-    <Flex minH={"100vh"}>
-      <Stack direction="column">
-        <Heading textAlign={"left"} p={10}>
-          Welcome, Username
-        </Heading>
-        <Stack direction={{ base: "column", md: "row" }}>
-          <ToDoSummary />
-          <Annoucements />
-        </Stack>
+    <Stack direction="column" minH={"100vh"}>
+      <Heading textAlign={"left"} p={10}>
+        Welcome, Username
+      </Heading>
+      <Stack direction={{ base: "column", lg: "row" }}>
+        <ToDoSummary />
+        <Annoucements />
       </Stack>
-    </Flex>
+    </Stack>
   );
 }
--- src/components/Dashboard/ToDoSummary.tsx
+++ src/components/Dashboard/ToDoSummary.tsx
@@ -1,106 +1,94 @@
-import { Fragment } from "react";
+import { useEffect, useState } from "react";
 import {
   Container,
   chakra,
   Flex,
   Stack,
-  VStack,
-  Grid,
-  Divider,
-  Link,
+  CircularProgress,
+  Text,
   useColorModeValue,
 } from "@chakra-ui/react";
+import { TasksInformation } from "@/lib/types";
+import { PostgrestError } from "@supabase/supabase-js";
+import { getTasks } from "@/lib/CRUD_Tasks";
 
-interface ItemAttributes {
-  title: string;
-  created_at: string;
+// Returns an Array of 3 items (number of Tasks, Completed tasks, incomplete tasks)
+function getTasksSummary(tasks: TasksInformation[] | null) {
+  let completedCounter = 0;
+  let incompleteCounter = 0;
+
+  if (tasks == null) {
+    return [0, 0, 0];
+  }
+  let len = tasks?.length;
+
+  for (let i = 0; i < len; i++) {
+    if (tasks[i].is_complete) {
+      completedCounter++;
+    } else {
+      incompleteCounter++;
+    }
+  }
+  return [len, completedCounter, incompleteCounter];
 }
 
-const items: ItemAttributes[] = [
-  {
-    title: "Task 1",
-    created_at: "21 Jan 2022",
-  },
-  {
-    title: "Create professional Web Application with Nextjs and ChakraUI",
-    created_at: "20 Jun 2021",
-  },
-  {
-    title: `Get Good Grades for University`,
-    created_at: "31 Sept 2022",
-  },
-];
-
 const ToDoSummary = () => {
-  const bgColorScheme = useColorModeValue("gray.200", "gray.700");
-  const otherStuffPlsExplainRichie = useColorModeValue("gray.600", "gray.300");
+  const [taskList, setTaskList] = useState<{
+    data: TasksInformation[] | null;
+    error: PostgrestError | null;
+  }>({
+    data: null,
+    error: null,
+  });
+
+  useEffect(() => {
+    getTasks(setTaskList);
+  }, []);
+
+  const taskSummaryArray = getTasksSummary(taskList.data);
+
   return (
-    <Container maxW="5xl" p={{ base: 5, md: 10 }}>
-      <Flex justify="center" mb={3}>
-        <chakra.h3 fontSize="2xl" fontWeight="bold" textAlign="center">
+    <Container maxW="5xl" p={{ base: 2, md: 10 }}>
+      <Flex justify="center" mb={3} gap={16}>
+        <chakra.h3 fontSize="2xl" fontWeight="bold">
           Tasks Summary
         </chakra.h3>
       </Flex>
-      <VStack
-        border="1px solid"
-        borderColor="gray.400"
-        rounded="md"
-        overflow="hidden"
-        spacing={0}
+      <Stack
+        bg={useColorModeValue("gray.50", "gray.700")}
+        direction={"row"}
+        align={"center"}
+        borderRadius={10}
+        borderWidth={2}
+        p={4}
+        justify={"center"}
       >
-        {items.map((item, index) => (
-          <Fragment key={index}>
-            <Grid
-              templateRows={{ base: "auto auto", md: "auto" }}
-              w="100%"
-              templateColumns={{ base: "5fr 3fr", md: "5fr 3fr" }}
-              p={{ base: 2, sm: 4 }}
-              gap={3}
-              alignItems="center"
-              _hover={{ bg: bgColorScheme }}
-            >
-              <Stack gridColumnEnd={{ base: "unset", md: "unset" }}>
-                <chakra.h3 fontWeight="bold" fontSize="lg">
-                  {item.title}
-                </chakra.h3>
-                <chakra.p
-                  fontWeight="medium"
-                  fontSize="sm"
-                  color={otherStuffPlsExplainRichie}
-                >
-                  Created: {item.created_at}
-                </chakra.p>
-              </Stack>
-              <Stack
-                spacing={2}
-                direction="row"
-                fontSize={{ base: "sm", sm: "md" }}
-                justifySelf="flex-end"
-                alignItems="center"
-              >
-                {["Manage", "Edit"].map((label, index) => (
-                  <ItemSettingLink key={index} label={label} />
-                ))}
-              </Stack>
-            </Grid>
-            {items.length - 1 !== index && <Divider m={0} />}
-          </Fragment>
-        ))}
-      </VStack>
+        <CircularProgress
+          value={80}
+          size={72}
+          color="green.400"
+          trackColor="gray.300"
+          thickness={12}
+        />
+        <Stack direction={"column"} pl={{ base: 5, md: 10 }}>
+          <Text fontSize={{ base: "lg", sm: "xl" }} fontWeight={"bold"}>
+            You have {taskSummaryArray[0]} tasks :
+          </Text>
+          <Text fontSize={{ base: "md", sm: "lg" }}>
+            <Text as="span" color={"green.300"} fontSize={"2xl"} pr={2}>
+              {taskSummaryArray[1]}
+            </Text>
+            Tasks Completed
+          </Text>
+          <Text fontSize={{ base: "md", sm: "lg" }}>
+            <Text as="span" color={"red.400"} fontSize={"2xl"} pr={2}>
+              {taskSummaryArray[2]}
+            </Text>
+            Tasks Incomplete
+          </Text>
+        </Stack>
+      </Stack>
     </Container>
-  );
-};
-
-const ItemSettingLink = ({ label }: { label: string }) => {
-  return (
-    <chakra.p
-      as={Link}
-      _hover={{ bg: useColorModeValue("gray.400", "gray.600") }}
-      p={1}
-      rounded="md"
-    >
-      {label}
-    </chakra.p>
   );
 };
 
