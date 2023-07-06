37ec447d9695f1e42c96b5ac1fedf20297d2579c
CJ-Lee01 <chunjie01@live.com>
2023-07-06 21:35:36 +0800

    Added canvas Sync route and fixed Canvas API calls.

--- src/lib/Canvas/CanvasAPI.ts
+++ src/lib/Canvas/CanvasAPI.ts
@@ -2,11 +2,12 @@ import { CanvasAPICourse, CanvasAPIAnnoucement, CanvasAPIAssignment } from "./Ca
 import { AnnouncementData, TasksInformation } from "../types";
 import { announcements, assignments } from "./MockCanvasData"
 import { resolve } from "path";
+import { access } from "fs";
 
 const ROOT_CANVAS_URL = "https://canvas.nus.edu.sg";
 const urlCourses = () => "/api/v1/users/self/courses?include[]=concluded";
 const urlAssignments = (courseID: number) => `/api/v1/users/self/courses/${courseID}/assignments?include[]=submission`;
-const urlAnnouncement = (courseID: number) => `api/v1/courses/${courseID}/discussion_topics?only_announcements=true`;
+const urlAnnouncement = (courseID: number) => `/api/v1/courses/${courseID}/discussion_topics?only_announcements=true`;
 const createCanvasAuthorizationHeader: (canvasToken: string) => { "Authorization": string } = (canvasToken: string) => {
   const header = new Headers();
   header.append("Authorization", `Bearer ${canvasToken}`);
@@ -23,37 +24,38 @@ export interface CourseInfo {
 }
 
 async function getCanvasCourses(canvasToken: string, includeConcluded: boolean = false): Promise<{ courses: CourseInfo[]; error: string | null; }> {
+  // By default, excludes courses that have already concluded.
   const header = createCanvasAuthorizationHeader(canvasToken);
   const result: { courses: CourseInfo[], error: string | null } = { courses: [], error: null };
-  const response = await fetch(`${ROOT_CANVAS_URL}`, {
+  const response = await fetch(`${ROOT_CANVAS_URL}${urlCourses()}`, {
     method: "GET",
     headers: header
-  }).then((response) => {
-    //if fulfilled
-    console.log(response)
-    console.log(response.status)
-    if (!response.ok) {
-      result.error = INVALID_CANVAS_TOKEN_ERROR;
-      return;
+  });
+  console.log(response.status)
+  if (response.status == 401) {
+    return {
+      courses: [],
+      error: "Error 401: Canvas Token given is not valid."
     }
-    console.log(response.json())
-    response.json()
-      .then(coursesList => (coursesList as CanvasAPICourse[]))
-      .then(courseList => courseList
-        .filter(course => includeConcluded ? course.concluded : true)
-        .forEach(course => course.name
-          ? result.courses.push({
-            courseName: course.name,
-            courseID: course.id,
-            courseCode: course.course_code ?? "Unknown Course Code"
-          })
-          : 1 //Do nothing if not valid.
-        ))
-  },
-    response => {
-      result.error = INVALID_CANVAS_TOKEN_ERROR;
-      return;
-    });
+  }
+
+  if (!response.ok) {
+    return {
+      courses: [],
+      error: "Error unknown. Please contact the developers with the details."
+    }
+  }
+
+  const data = (await response.json());
+  (data as CanvasAPICourse[]).filter(course => includeConcluded ? true : !course.concluded)
+  .forEach(course => course.name
+    ? result.courses.push({
+      courseName: course.name,
+      courseID: course.id,
+      courseCode: course.course_code ?? "Unknown Course Code"
+    })
+    : 1 //Do nothing if not valid.
+  )
   return result;
 
 }
@@ -63,7 +65,6 @@ async function getCanvasAnnouncements(canvasToken: string, courseList: CourseInf
   const header = createCanvasAuthorizationHeader(canvasToken);
   const fetchArray: Promise<any>[] = courseList
     .map(course => fetch(`${ROOT_CANVAS_URL}${urlAnnouncement(course.courseID)}`, {
-      mode: 'no-cors',
       method: "GET",
       headers: header
     }).then(response => {
@@ -73,9 +74,15 @@ async function getCanvasAnnouncements(canvasToken: string, courseList: CourseInf
       }
       response.json()
         .then(announcementList => (announcementList as CanvasAPIAnnoucement[]))
+        .then(announcementList => announcementList.filter(item => {
+          const announcedDate = new Date(item.posted_at ?? Date.now())
+            console.log(announcedDate.toLocaleString())
+            return !(startDate && announcedDate < startDate)
+        }))
         .then(announcementList => {
           announcementList.forEach(announcement => {
             const announcedDate = new Date(announcement.posted_at ?? Date.now())
+            console.log(announcedDate.toLocaleString())
             if (startDate && announcedDate < startDate) {
               return
             }
@@ -90,8 +97,6 @@ async function getCanvasAnnouncements(canvasToken: string, courseList: CourseInf
         })
     }))
 
-  await Promise.all(fetchArray);
-
   return result;
 }
 
@@ -100,7 +105,6 @@ async function getCanvasAssignments(canvasToken: string, courseList: CourseInfo[
   const header = createCanvasAuthorizationHeader(canvasToken);
   const fetchArray: Promise<any>[] = courseList
     .map(course => fetch(`${ROOT_CANVAS_URL}${urlAssignments(course.courseID)}`, {
-      mode: 'no-cors',
       method: "GET",
       headers: header
     }).then(response => {
