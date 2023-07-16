ebf5fc19183540c0c3eb10b54ca56e0c0e8e19ad
CJ-Lee01 <chunjie01@live.com>
2023-07-16 23:26:18 +0800

    Added new test file.

---
+++ src/components/ToDoList/__test__/TaskComponent.test.ts
@@ -0,0 +1,6 @@
+import TaskComponent from "../TaskComponent";
+import React, {ReactElement} from 'react'
+import {render, RenderOptions} from '@testing-library/react'
+import { tasks } from "./TestValues";
+
+
