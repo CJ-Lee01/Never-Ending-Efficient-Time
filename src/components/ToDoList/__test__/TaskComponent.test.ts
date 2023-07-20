f07f2066017905a0305eaa67d5fb351c4a95de39
CJ-Lee01 <chunjie01@live.com>
2023-07-20 14:33:19 +0800

    Removed empty test file.

--- src/components/ToDoList/__test__/TaskComponent.test.ts
+++
@@ -1,7 +0,0 @@
-import TaskComponent from "../TaskComponent";
-import React, { ReactElement } from "react";
-import { render, RenderOptions } from "@testing-library/react";
-
-it("buffer", () => {
-  expect(true);
-});
